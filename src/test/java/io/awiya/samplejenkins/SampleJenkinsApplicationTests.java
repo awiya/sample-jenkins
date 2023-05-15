9e0636acbcdf9b98b0c55c50d84d2792c5b41fff
AYOUB <a.idihia@gmail.com>
2023-05-15 11:44:21 +0200

    Initial commit

---
+++ src/test/java/io/awiya/samplejenkins/SampleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awiya.samplejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
